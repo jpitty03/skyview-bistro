abc703b57257be901244384ea602903096e96b64
Josh Fairbank <jpitty03@gmail.com>
2025-12-12 11:20:53 -0800

    added workflow, ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import logo from './assets/Skyview_Bistro_logo_small.png'
+import logo from './assets/Skyview_Bistro_logo_small_dark_gray.png'
 
 function App() {
   return (
     <div className="coming-soon-container">
       <div className="content">
         <img src={logo} className="logo" alt="Skyview Bistro and Lounge" />
-        <h1 className="coming-soon-text">Coming Soon</h1>
-        <h2 className="restaurant-name">Skyview Bistro and Lounge</h2>
+        <h2 className="coming-soon-text">Coming Soon</h2>
+        {/* <h2 className="restaurant-name">Skyview Bistro and Lounge</h2> */}
         <p className="tagline">An elevated dining experience is on the horizon</p>
       </div>
     </div>
